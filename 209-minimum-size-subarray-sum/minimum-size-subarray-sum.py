5e5979b2c572ff0ada0b62473fe64cd10c02136f
Yared Gebre <yaredgebre120@gmail.com>
2026-03-21 05:39:49 -0300

    Time: 7 ms (94.39%) | Memory: 30.6 MB (63.58%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -4,15 +4,11 @@ class Solution:
        j = 0
        sum_1 = 0
        for i in range(len(nums)):
-            sum_1 += nums[i]
-            if sum_1 >= target:
-                output = min(i+1 - j, output)
-                while target < sum_1:
+                sum_1 += nums[i]
+                while target <= sum_1:
+                    output = min(i+1 - j, output)
                     sum_1 -= nums[j]
-                    j+=1
-                    if sum_1 >= target:
-                        output = min(i+1 - j, output)
-                    
+                    j+=1         
        if output == float("inf"):
         return 0
        else: 
