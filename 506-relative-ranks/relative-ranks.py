b2825d79b7cbf1d627260c6704f17d8d7df36f21
Yared Gebre <yaredgebre120@gmail.com>
2026-03-24 04:45:59 -0300

    Time: 7 ms (54.62%) | Memory: 20.3 MB (80.27%) - LeetSync

--- 506-relative-ranks/relative-ranks.py
+++ 506-relative-ranks/relative-ranks.py
@@ -1,21 +1,19 @@
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
-        value = []
+        hash_map = {}
         score1 = sorted(score)
         output = []
         for i in range(len(score1)):
             if len(score1)-i == 3:
-                 value.append("Bronze Medal")
+                 hash_map[score1[i]] = "Bronze Medal"
             elif len(score1)-i == 2:
-                 value.append("Silver Medal")
+                 hash_map[score1[i]] = "Silver Medal"
             elif len(score1)-i == 1:
-                 value.append("Gold Medal")
+                 hash_map[score1[i]] = "Gold Medal"
             else:
-                value.append(str(len(score1) - i))
-        for j in range(len(score)):
-            for i in range(len(score1)):
-                if score[j] == score1[i]:
-                    output.append(value[i])
+                hash_map[score1[i]] = (str(len(score1) - i))
+        for j in score:
+            output.append(hash_map[j])
         return output
 
 
