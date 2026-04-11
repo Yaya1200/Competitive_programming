4548d5733a87b8dceae97dd2215dc235e6bc9838
Yared Gebre <yaredgebre120@gmail.com>
2026-04-11 05:45:24 -0300

    Time: 31 ms (57.59%) | Memory: 33.9 MB (44.73%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,21 +1,15 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        output = []
-        hash_map = []
-        for i in nums:
-            if i < pivot:
-                hash_map.append([i, -1])
-            elif i > pivot:
-                hash_map.append([i, 1])
+        less = []
+        equal = []
+        greater = []
+        for num in nums:
+            if num < pivot:
+                less.append(num)
+            elif num > pivot:
+                greater.append(num)
             else:
-                hash_map.append([i, 0])
-        for i in hash_map:
-            if i[1] == -1:
-                output.append(i[0])
-        for i in hash_map:
-            if i[1] == 0:
-                output.append(i[0])
-        for i in hash_map:
-            if i[1] == 1:
-                output.append(i[0])
-        return output
+                equal.append(num)
+        less.extend(equal)
+        less.extend(greater)
+        return less
