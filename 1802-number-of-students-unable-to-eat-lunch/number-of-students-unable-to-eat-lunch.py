79c10ebe5047239cefa0898be551bfa76c6321f9
Yared Gebre <yaredgebre120@gmail.com>
2026-05-21 12:14:44 -0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.09%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        i = 0
+        count = 0
+        j = 0
+        while(True):
+            if students[i] == sandwiches[j]:
+                students = students[:i] + students[i+1:]
+                count =0
+                j += 1
+            else:
+                count += 1
+            i += 1
+            if count == len(students)+1:
+                return len(students)
+            if len(students) == 0:
+                return 0
+            i =  i % len(students)
+
