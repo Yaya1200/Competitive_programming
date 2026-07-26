45eab0b68d6c2d8641ae161c05bab8f9a38adcb1
Yared Gebre <yaredgebre120@gmail.com>
2026-07-26 10:19:30 -0300

    Time: 0 ms (100.00%) | Memory: 20.3 MB (78.26%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,33 +2,27 @@ class Solution:
     def canFinish(self, numCourses, prerequisites):
 
         indegree = {}
-
+        Queue = deque()
         for i in range(numCourses):
             indegree[i] = 0
-
+        graph = [[] for _ in range(numCourses)]
+        for course, pre in prerequisites:
+              graph[pre].append(course)
         for course, pre in prerequisites:
             indegree[course] += 1
 
+        for i in indegree:
+            if indegree[i]== 0:
+                Queue.append(i)
+        if not Queue:
+            return False
+        
         processed = 0
-
-        while True:
-
-            found = False
-
-            for course in indegree:
-
-                if indegree[course] == 0:
-
-                    found = True
-                    processed += 1
-
-                    indegree[course] = -1
-
-                    for c, p in prerequisites:
-                        if p == course:
-                            indegree[c] -= 1
-
-            if not found:
-                break
-
+        while Queue:
+            course = Queue.popleft()
+            processed += 1
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    Queue.append(neighbor)
         return processed == numCourses
