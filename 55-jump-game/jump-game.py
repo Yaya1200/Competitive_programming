da21d69955c05669cc6e70b931f529730cb61c95
Yared Gebre <yaredgebre120@gmail.com>
2026-08-05 15:57:15 +0300

    Time: 23 ms (43.05%) | Memory: 20.4 MB (46.21%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,20 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        queue = deque()
-        visited = set()
-
-        queue.append([nums[0], 0])
-        visited.add(0)
-
-        while queue:
-            value, index = queue.popleft()
-
-            for i in range(index+1, min(index + value + 1, len(nums))):
-                if i == len(nums)-1:
-                    return True
-
-                if i not in visited:
-                    queue.append([nums[i], i])
-                    visited.add(i)
-
-        return len(nums) == 1
+        last = 0
+        for i in range(len(nums)):
+            if i > last:
+                return False
+            last = max(last, i+nums[i])
+            
+        return True
