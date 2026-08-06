d206091cf9eb8a3cd67261568ef388fa2383295a
Yared Gebre <yaredgebre120@gmail.com>
2026-08-06 14:09:49 +0300

    Time: 11 ms (10.96%) | Memory: 23.5 MB (38.29%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -2,9 +2,9 @@ class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         hash_map = {5:0, 10:0, 20:0}
         for i in bills:
-            hash_map[i] += 1
             value = i - 5
-            while value > 0 :
+
+            while value > 0:
                 if value >= 10 and hash_map[10] > 0:
                     hash_map[10] -= 1
                     value -= 10
@@ -12,8 +12,8 @@ class Solution:
                     hash_map[5] -= 1
                     value -= 5
                 else:
-                    break
-            if value > 0:
-                return False
-        return True
+                    return False
 
+            hash_map[i] += 1
+
+        return True
