1bcfab1311ab70aa4e320267dfa81baa3bac4926
Yared Gebre <yaredgebre120@gmail.com>
2026-08-19 11:27:48 +0300

    Time: 60 ms (15.62%) | Memory: 21 MB (30.49%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -9,20 +9,18 @@ class Solution:
            else:
              output1.append(arr[i])
        output1.sort(reverse=True)
-       heapify(output2)
-       print(output1)
-       print(output2)
+       output2.sort()
        while (output1 or output2) and len(result) < k:
           if output1 and output2:
             if abs(output1[0] - x) <= abs(output2[0] - x):
                 result.append(output1.pop(0))
             else:
-                result.append(heapq.heappop(output2))
+                result.append(output2.pop(0))
           else:
              if output1:
                 result.append(output1.pop(0))
              else:
-                result.append(heapq.heappop(output2))
+                result.append(output2.pop(0))
 
        return sorted(result)
          
